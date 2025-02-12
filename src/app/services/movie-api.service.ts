45e7e7b34aeedbf8de0f68387484ea6ae20ccc3d
unknown <overaisulislam@gmail.com>
2025-02-13 01:26:25 +0600

    Done

--- src/app/services/movie-api.service.ts
+++ src/app/services/movie-api.service.ts
@@ -7,13 +7,12 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class MovieApiService {
-  private accessToken = environment.AccessToken;
   constructor(private http: HttpClient) {}
 
   getPopularMovies(page: number = 1): Observable<any> {
     const headers = new HttpHeaders({
       accept: 'application/json',
-      Authorization: `Bearer ${this.accessToken}`,
+      Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmYmQwMWZhYjE0NTMyZmQ2OWYxZTZhNjA3MGFmMjYyNSIsIm5iZiI6MTczOTM3NzE5NC4wNTcsInN1YiI6IjY3YWNjYTJhM2VlNzA4MzI3YzM2Y2U4ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.5Gx1vxbBVzDnX5PV0dVASwnXjgKJXeiEMpFBLmeV5Rc`,
     });
     return this.http.get<any>(
       `https://api.themoviedb.org/3/movie/popular?page=${page}`,
