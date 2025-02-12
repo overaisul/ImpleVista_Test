c4ff7172becf872a657fe7e895904f3f80214038
unknown <overaisulislam@gmail.com>
2025-02-13 00:46:09 +0600

    Done

--- src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  ApiKey: 'fbd01fab14532fd69f1e6a6070af2625',
-  AccessToken:
-    'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmYmQwMWZhYjE0NTMyZmQ2OWYxZTZhNjA3MGFmMjYyNSIsIm5iZiI6MTczOTM3NzE5NC4wNTcsInN1YiI6IjY3YWNjYTJhM2VlNzA4MzI3YzM2Y2U4ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.5Gx1vxbBVzDnX5PV0dVASwnXjgKJXeiEMpFBLmeV5Rc',
-  ApiUrl: 'https://api.themoviedb.org/3',
-};
