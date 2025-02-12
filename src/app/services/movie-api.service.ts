c2b40a8953b2d3b11663c6e082c00647c6e6b1cb
unknown <overaisulislam@gmail.com>
2025-02-13 00:51:24 +0600

    Done

--- src/app/services/movie-api.service.ts
+++ src/app/services/movie-api.service.ts
@@ -8,7 +8,6 @@ import { Observable } from 'rxjs';
 })
 export class MovieApiService {
   private apiUrl = environment.ApiUrl;
-  private apiKey = environment.ApiKey;
   private accessToken = environment.AccessToken;
   constructor(private http: HttpClient) {}
 
